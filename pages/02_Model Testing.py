f16b02f692003af1a7cec562216c8834ea1d972b
Samuel Nicasio <marksamuel.nicasio@gmail.com>
2024-06-08 10:08:33 +0800

    RandomForestClassifier

--- pages/02_Model Testing.py
+++ pages/02_Model Testing.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 with open("random_forests_hypertuned.pkl", "rb") as f:
     model = pickle.load(f)
