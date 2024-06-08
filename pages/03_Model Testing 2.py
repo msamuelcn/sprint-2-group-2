262a4a05fd7be554cdd67d0f757a85bfc927a3be
Samuel Nicasio <marksamuel.nicasio@gmail.com>
2024-06-08 11:07:56 +0800

    model fitting

--- pages/03_Model Testing 2.py
+++ pages/03_Model Testing 2.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pickle
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 # pickle_in = open("random_forests_hypertuned.pkl", "rb")
 # model = pickle.load(pickle_in)
@@ -22,6 +24,13 @@ for col in df_testing.columns:
 # Define features (X) and target(y)
 X = df_testing.drop(['IS_POVERTY'], axis=1) # feature, remove is_poverty
 y = df_testing['IS_POVERTY'] # target
+# Split the data
+X_trainval, X_holdout, y_trainval, y_holdout = train_test_split(X, y, random_state=1337, test_size=0.25,
+                                                                  stratify=y)
+
+model = RandomForestClassifier(max_depth=8, max_features='log2', n_estimators=200,
+                       random_state=1337, n_jobs=-1)
+model.fit(X_trainval, y_trainval)
 
 
 option = st.selectbox(
@@ -31,9 +40,9 @@ option = st.selectbox(
 
 st.write("You selected:", option)
 
-# if st.button('Predict the household'):
-#     predictIsPoverty = predict(X.loc[option],model)
+if st.button('Predict the household'):
+    predictIsPoverty = predict(X.loc[option],model)
 
-#     predictIsPovertyWord = 'positive' if predictIsPoverty ==1 else 'negative'
-#     st.success(f'The predicted household no.'+ option+' is '+predictIsPovertyWord+'.')
+    predictIsPovertyWord = 'positive' if predictIsPoverty ==1 else 'negative'
+    st.success(f'The predicted household no.'+ option+' is '+predictIsPovertyWord+'.')
 
