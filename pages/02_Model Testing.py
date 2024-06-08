e18dbfaed43c259256d9e857bc7bb54facda6be5
Samuel Nicasio <marksamuel.nicasio@gmail.com>
2024-06-08 11:33:47 +0800

    model testing

--- pages/02_Model Testing.py
+++ pages/02_Model Testing.py
@@ -1,47 +1,49 @@
 import streamlit as st
 import pickle
 import pandas as pd
-# import sklearn
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.tree import DecisionTreeClassifier
 
-pickle_in = open("random_forests_hypertuned.pkl", "rb")
-model = pickle.load(pickle_in)
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
+# pickle_in = open("random_forests_hypertuned.pkl", "rb")
+# model = pickle.load(pickle_in)
 
-#Caching the model for faster loading
-# @st.cache
-
-# Define the prediction function
-# def predict(X_data, m):
-#     prediction = m.predict(X_data)
-#     return prediction
+def predict(X_data, m):
+    prediction = m.predict(pd.DataFrame(X_data))
+    return prediction
 
 
 st.title('Impoverished Group Predictor')
-#st.header('Enter the characteristics of the diamond:')
-#carat = st.number_input('Carat Weight:', min_value=0.1, max_value=10.0, value=1.0)
-#cut = st.selectbox('Cut Rating:', ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
-#color = st.selectbox('Color Rating:', ['J', 'I', 'H', 'G', 'F', 'E', 'D'])
-#clarity = st.selectbox('Clarity Rating:', ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'])
-#depth = st.number_input('Diamond Depth Percentage:', min_value=0.1, max_value=100.0, value=1.0)
-#table = st.number_input('Diamond Table Percentage:', min_value=0.1, max_value=100.0, value=1.0)
-#x = st.number_input('Diamond Length (X) in mm:', min_value=0.1, max_value=100.0, value=1.0)
-#y = st.number_input('Diamond Width (Y) in mm:', min_value=0.1, max_value=100.0, value=1.0)
-#z = st.number_input('Diamond Height (Z) in mm:', min_value=0.1, max_value=100.0, value=1.0)
 
 df = pd.read_csv('data/20240602_fies_cleaned_.csv')
+to_drop = ['SEQUENCE_NO']
+df_testing = df.drop(to_drop, axis=1)
+for col in df_testing.columns:
+  print(col)
 
+# Define features (X) and target(y)
+X = df_testing.drop(['IS_POVERTY'], axis=1) # feature, remove is_poverty
+y = df_testing['IS_POVERTY'] # target
+# Split the data
+X_trainval, X_holdout, y_trainval, y_holdout = train_test_split(X, y, random_state=1337, test_size=0.25,
+                                                                  stratify=y)
 
-option = st.selectbox(
-    "Select a houshold number.",
-    tuple(df['SEQUENCE_NO'])
-    )
+model = RandomForestClassifier(max_depth=8, max_features='log2', n_estimators=200,
+                       random_state=1337, n_jobs=-1)
+model.fit(X_trainval, y_trainval)
+predicted = model.predict(X)
 
-st.write("You selected:", option)
+# option = st.selectbox(
+#     "Select a houshold number.",
+#     tuple(df['SEQUENCE_NO'])
+#     )
+option  = st.number_input('Select a houshold number.', min_value=1, max_value=len(X), value=1)
 
-# if st.button('Predict Price'):
-#     price = predict(X_data,model)
-#     st.success(f'The predicted price of the diamond is ${price[0]:.2f} USD')
+# st.write("You selected:", option)
 
+if st.button('Predict the household'):
+    predictIsPoverty = predicted[option-1]
+
+    predictIsPovertyWord = 'positive' if predictIsPoverty ==1 else 'negative'
+    st.success(f'The predicted household no.'+ str(option)+' is '+predictIsPovertyWord+'.')
 
