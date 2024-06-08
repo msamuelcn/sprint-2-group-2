6f00a1f92be54adeab3485a712527bb8c7ca7d97
Samuel Nicasio <marksamuel.nicasio@gmail.com>
2024-06-08 10:16:04 +0800

    DecisionTreeClassifier

--- pages/02_Model Testing.py
+++ pages/02_Model Testing.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 with open("random_forests_hypertuned.pkl", "rb") as f:
     model = pickle.load(f)
